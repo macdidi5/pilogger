006899035fd7db7fc92eac4d47121b591544a368
muth.inc <muth.inc@gmail.com>
2013-10-01 12:22:02 +0000

    Upload Day and month data to FTP

--- pilogger/src/pilogger/UploadFTP.java
+++ pilogger/src/pilogger/UploadFTP.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.SocketException;
+import java.nio.file.Path;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -22,12 +23,30 @@ public class UploadFTP {
 			return;
 		}
 		
-		ftp.login("muth.inc", "gx88sodw");
+		ftp.login("muth.inc", "********");
 		ftp.setFileType(FTP.BINARY_FILE_TYPE);
 		ftp.enterLocalPassiveMode();
 		
 		InputStream input = new FileInputStream(new File(localPath+localFileName));
 		ftp.storeFile("pilogger/" + localFileName, input);
+		ftp.disconnect();
+	}
+	
+public static synchronized void store(Path localFilePath) throws SocketException, IOException {
+		
+		ftp.connect(HOSTNAME);
+		if (! FTPReply.isPositiveCompletion(  ftp.getReplyCode() )) {
+			ftp.disconnect();
+			return;
+		}
+		
+		ftp.login("muth.inc", "gx88sodw");
+		ftp.setFileType(FTP.BINARY_FILE_TYPE);
+		ftp.enterLocalPassiveMode();
+		
+		InputStream input = new FileInputStream(localFilePath.toFile());
+		ftp.storeFile("pilogger/" + localFilePath.getFileName(), input);
+		ftp.disconnect();
 	}
 
 }
