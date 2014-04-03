95268067e7e85af5a08d0354c50a8250be6415e6
muth.inc <muth.inc@gmail.com>
2014-04-03 20:25:54 +0000

    change ftp uploading

--- pilogger/src/pilogger/UploadFTP.java
+++ pilogger/src/pilogger/UploadFTP.java
@@ -1,6 +1,5 @@
 package pilogger;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,41 +11,38 @@ import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
 public class UploadFTP {
-	private static final FTPClient ftp = new FTPClient();
-	private static final String HOSTNAME = "ftpperso.free.fr";
-	
-	public static synchronized void store(String localFileName, String localPath) throws SocketException, IOException {
-		
-		ftp.connect(HOSTNAME);
-		if (! FTPReply.isPositiveCompletion(  ftp.getReplyCode() )) {
+	private static final String HOSTNAME = "xx.xx.xx";
+
+	public static synchronized void store(Path localFilePath) {
+		FTPClient ftp = new FTPClient();
+		try {
+			ftp.connect(HOSTNAME);
+			
+			if (! FTPReply.isPositiveCompletion(  ftp.getReplyCode() )) {
+				ftp.disconnect();
+				return;
+			}
+
+			ftp.login("xxx", "yyy");
+			ftp.setFileType(FTP.BINARY_FILE_TYPE);
+			ftp.enterLocalPassiveMode();
+
+			InputStream input = new FileInputStream(localFilePath.toFile());
+			ftp.storeFile("pilogger/" + localFilePath.getFileName(), input);
+			ftp.logout();
 			ftp.disconnect();
-			return;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch(IOException ioe) {
+					// do nothing
+				}
+			}
 		}
-		
-		ftp.login("muth.inc", "********");
-		ftp.setFileType(FTP.BINARY_FILE_TYPE);
-		ftp.enterLocalPassiveMode();
-		
-		InputStream input = new FileInputStream(new File(localPath+localFileName));
-		ftp.storeFile("pilogger/" + localFileName, input);
-		ftp.disconnect();
-	}
-	
-public static synchronized void store(Path localFilePath) throws SocketException, IOException {
-		
-		ftp.connect(HOSTNAME);
-		if (! FTPReply.isPositiveCompletion(  ftp.getReplyCode() )) {
-			ftp.disconnect();
-			return;
-		}
-		
-		ftp.login("muth.inc", "********");
-		ftp.setFileType(FTP.BINARY_FILE_TYPE);
-		ftp.enterLocalPassiveMode();
-		
-		InputStream input = new FileInputStream(localFilePath.toFile());
-		ftp.storeFile("pilogger/" + localFilePath.getFileName(), input);
-		ftp.disconnect();
+
 	}
 
 }
